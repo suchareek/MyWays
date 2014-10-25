f750a13d2b3906b3f0e29eb287ad574843450e34
Jakub Laskowski <jakublas92@gmail.com>
2014-10-25 14:04:13 +0200

    i02t02 - pokazywanie lokalozacji uzytkownika na mapie, wyswietlanie okienka do dodawania punktów

--- MyWays/src/com/example/myways/MainActivity.java
+++ MyWays/src/com/example/myways/MainActivity.java
@@ -3,8 +3,6 @@ package com.example.myways;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -17,39 +15,14 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		
-		
 		launchMap = (Button) findViewById(R.id.launchMap);
 		
-		launchMap.setOnClickListener(new View.OnClickListener() {
-		    public void onClick(View v) {
-		    	
-		    	Intent j = new Intent(MainActivity.this, MapActivity.class);
-				startActivity(j);
-		        
-		    }
-		});
-		
 	}
 	
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
+	public void launchMap(View v)
+	{
+		Intent j = new Intent(MainActivity.this, MapActivity.class);
+		startActivity(j);
 	}
 	
 	@Override
--- MyWays/src/com/example/myways/MapActivity.java
+++ MyWays/src/com/example/myways/MapActivity.java
@@ -2,20 +2,34 @@ package com.example.myways;
 
 
 
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapActivity extends FragmentActivity {
 	
 	private GoogleMap map;
-	private Button viewChangeButton;
+	private Button viewChangeButton, addPointButton;
+	private int userIcon;
+	private double lat,lng;
+	private LocationManager mlocManager;
+	private MyLocationListener mlocListener;
+	private Marker userMarker;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -23,31 +37,95 @@ public class MapActivity extends FragmentActivity {
 		setContentView(R.layout.activity_map);
 		
 		viewChangeButton = (Button) findViewById(R.id.viewChange);
+		addPointButton = (Button) findViewById(R.id.addPoint);
 		
 		map = ((SupportMapFragment)  getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
 		
 		map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
 		viewChangeButton.setText("Normalna");
 		
-		map.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(50.061452, 19.937925),12.0f), 3000, null);
+		map.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(50.061452, 19.937925),12.0f), 1000, null);
 		
-		viewChangeButton.setOnClickListener(new View.OnClickListener() {
-		    public void onClick(View v) {
-		    	
-		    	if (map.getMapType()==GoogleMap.MAP_TYPE_HYBRID)
-		    	{
-		    		map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-		    		viewChangeButton.setText("Hybryda");
-		    	}
-		    	else
-		    	{
-		    		map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-		    		viewChangeButton.setText("Normalna");
-		    	}
-		        
-		    }
-		});
+		userIcon = R.drawable.yellow_point;
 		
+		mlocManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+		
+		mlocListener = new MyLocationListener();
+
+		mlocManager.requestLocationUpdates( LocationManager.GPS_PROVIDER, 1000, 0, mlocListener);
+		
+	}
+	
+	public void viewChange(View v)
+	{
+		if (map.getMapType()==GoogleMap.MAP_TYPE_HYBRID)
+    	{
+    		map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+    		viewChangeButton.setText("Hybryda");
+    	}
+    	else
+    	{
+    		map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+    		viewChangeButton.setText("Normalna");
+    	}
+	}
+	
+	public void addPoint(View v)
+	{
+		Intent j = new Intent(MapActivity.this, NewPoint.class);
+		startActivity(j);
+	}
+	
+	private void updatePlaces()
+	{
+		Location lastLoc = mlocManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+		
+		LatLng lastLatLng = new LatLng(lat,lng);
+		
+		if(userMarker!=null){
+			userMarker.remove();
+		}
+		
+		userMarker = map.addMarker(new MarkerOptions()
+	    	.position(lastLatLng)
+	    	.title("Your last position")
+	    	.icon(BitmapDescriptorFactory.fromResource(userIcon))
+	    	.snippet(""));
+		
+		map.animateCamera(CameraUpdateFactory.newLatLng(lastLatLng), 1000, null);
+		
+	}
+	
+	public class MyLocationListener implements LocationListener
+	{
+
+		@Override
+		public void onLocationChanged(Location loc)
+		{
+			lat = loc.getLatitude();
+			lng = loc.getLongitude();
+			
+			updatePlaces();
+			
+		}
+		
+		@Override
+		public void onProviderDisabled(String provider)
+		{
+			Toast.makeText( getApplicationContext(),"Gps Disabled",	Toast.LENGTH_SHORT ).show();
+		}
+		
+		@Override
+		public void onProviderEnabled(String provider)
+		{
+			Toast.makeText( getApplicationContext(),"Gps Enabled",Toast.LENGTH_SHORT).show();
+		}
+
+		@Override
+		public void onStatusChanged(String provider, int status, Bundle extras)
+		{
+			
+		}
 		
 	}
 
